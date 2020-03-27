97dec32335fe0915c388c0f5bfee7fd5f1a3912e
Eric Angenault <eric@angenault.net>
2020-03-27 21:40:13 +0100

    maj

--- system/wifi.py
+++ system/wifi.py
@@ -1,9 +1,8 @@
 import os
 import SendData
 
-q =  float( os.popen("awk 'NR==3 {print $3 }' /proc/net/wireless").read().replace(' ','').replace('\n', '').replace('\r', '')) 
-q = 52.5
-print('(',q,')')
+q =  round( float (os.popen("awk 'NR==3 {print $3 }' /proc/net/wireless").read().replace(' ','').replace('\n', '').replace('\r', '')) )
+print(q)
 
 SendData.mqtt("system", "wifi/quality", q)
 SendData.influxDB("system", "wifi/quality", q)
