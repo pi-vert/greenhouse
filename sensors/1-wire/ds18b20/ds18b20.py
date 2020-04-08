7341cf2ec967fd514b81937732ce0515f982f99c
Eric Angenault <eric@angenault.net>
2020-04-08 21:27:33 +0200

    ds18b20

--- sensors/1-wire/ds18b20/ds18b20.py
+++ sensors/1-wire/ds18b20/ds18b20.py
@@ -1,6 +1,5 @@
 import sys
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
+import SendData
 
 def lireFichier (emplacement) :
     fichTemp = open(emplacement)
@@ -15,31 +14,20 @@ def recupTemp (contenuFich) :
     temperature = temperature / 1000
     return temperature
 
-def publish (sensor, measurement, value) :
-    import paho.mqtt.client as mqtt
-    import json
-    client = mqtt.Client()
-    client.connect('localhost', 1883, 30)
-    client.loop_start()
-    client.publish( sensor, json.dumps( {measurement: value }), 1)
-    client.loop_stop()
-    client.disconnect()
-    return
-
-def store (sensor, measurement, value) :
-    from influxdb_client import InfluxDBClient, Point
-    from influxdb_client.client.write_api import SYNCHRONOUS
-    client = InfluxDBClient.from_config_file("/home/pi/influxdb.ini")
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-    query_api = client.query_api()
-    p = Point(measurement).tag("source", "vert").field(sensor, value)
-    write_api.write(bucket="greenhouse", org="eric@angenault.net", record=p)
-    return 
-
-contenuFich = lireFichier("/sys/bus/w1/devices/28-01192108919d/w1_slave")
+contenuFich = lireFichier("/sys/bus/w1/devices/28-0119113a3b60/w1_slave")
 temperature = recupTemp (contenuFich)
-print ("Temperature: ", temperature)
+print ("Temperature_Y: ", temperature)
 
-publish('sensors/ds18b20/1', 'temperature', temperature)
-store('sensors/ds18b20/1', 'temperature', temperature)
- 
+SendData.state('sensors/ds18b20', 'temperature1', temperature)
+
+contenuFich = lireFichier("/sys/bus/w1/devices/28-01191ae5edd9/w1_slave")
+temperature = recupTemp (contenuFich)
+print ("Temperature_G: ", temperature)
+
+SendData.state('sensors/ds18b20', 'temperature1', temperature)
+
+contenuFich = lireFichier("/sys/bus/w1/devices/28-011921255a5b/w1_slave")
+temperature = recupTemp (contenuFich)
+print ("Temperature_R: ", temperature)
+
+SendData.state('sensors/ds18b20', 'temperature3', temperature)
